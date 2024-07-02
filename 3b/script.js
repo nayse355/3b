7c155d261c7cf86c9fe6ead2e31690587e273a74
nayse355 <portela.emeli@escola.pr.gov.br>
2024-07-01 22:43:46 -0300

    Add files via upload

--- 3b/script.js
+++ 3b/script.js
@@ -1,1 +1,24 @@
-const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPrincipal = document.querySelector(" .caixa-principal");
+
+const caixaPerguntas = document.querySelector(" .caixa-perguntas");
+
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+
+const caixaResultado = documentquerySelector(" .caixa-resultado");
+
+const textoResultado = document.querySelector(" .texto-resultado");
+
+const perguntas = [ 
+    {
+        enunciado: "Insira o enunciado da Pergunta 1",
+        alternativas: ["Alternativa 1", "alternativa 2"], 
+    },
+    { 
+        enunciado: "Insira o enunciado da Pergunta 2",
+        alternativas: ["Alternativa 1", "alternativa 2"],    
+    }
+    {
+        enunciado: "Insira o enunciado da Pergunta 3",
+        alternativas: ["Alternativa 1", "alternativa 2"],  
+    },
+    ]; 
